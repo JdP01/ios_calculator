476cda69a3754009d95a0d9873adbd0efc7e5c89
juandiego <juandipleitez04@gmail.com>
2025-03-01 10:23:43 -0700

    first version calculator

---
+++ first_calculator/first_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_calculatorApp.swift
+//  first_calculator
+//
+//  Created by Juan Diego Pleitez on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct first_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
